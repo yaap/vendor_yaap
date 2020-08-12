d982199c5ecd44371c674adfd3522188bc168d99
Kshitij Gupta <kshitijgm@gmail.com>
2020-08-12 16:48:36 +0530

    [DNM] soong: Fully override path for now
    - Might break kernel build, but fixes syntax errors. Parsing may have changed.
    
    Change-Id: I2fb68146655e884d137a6eada0990de3a6b04f33
    
    -- Also define and export make variables used in new command

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -1,5 +1,6 @@
-PATH_OVERRIDE_SOONG := $(shell echo $(TOOLS_PATH_OVERRIDE) | sed -e 's|$$|$$$$|g')
-
+ifneq (,$(wildcard $(OUT_DIR)/.path_interposer_origpath))
+ORIG_PATH := $(shell cat $(OUT_DIR)/.path_interposer_origpath)
+endif
 # Add variables that we wish to make available to soong here.
 EXPORT_TO_SOONG := \
     KERNEL_ARCH \
@@ -9,19 +10,23 @@ EXPORT_TO_SOONG := \
     KERNEL_MAKE_FLAGS \
     PATH_OVERRIDE_SOONG \
     TARGET_KERNEL_CONFIG \
-    TARGET_KERNEL_SOURCE
+    TARGET_KERNEL_SOURCE \
+	  ORIG_PATH \
+    KERNEL_CLANG_TRIPLE \
+    KERNEL_CC \
+    MAKE_PREBUILT 
 
 # Setup SOONG_CONFIG_* vars to export the vars listed above.
 # Documentation here:
 # https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
 
-SOONG_CONFIG_NAMESPACES += yaapVarsPlugin
+SOONG_CONFIG_NAMESPACES += aosipVarsPlugin
 
-SOONG_CONFIG_yaapVarsPlugin :=
+SOONG_CONFIG_aosipVarsPlugin :=
 
 define addVar
-  SOONG_CONFIG_yaapVarsPlugin += $(1)
-  SOONG_CONFIG_yaapVarsPlugin_$(1) := $$(subst ",\",$$($1))
+  SOONG_CONFIG_aosipVarsPlugin += $(1)
+  SOONG_CONFIG_aosipVarsPlugin_$(1) := $$(subst ",\",$$($1))
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
