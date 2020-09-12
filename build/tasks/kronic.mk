cecfd91e72f01b9733353d589da295aa65c75ca1
Adhitya Mohan <me@adhityamohan.in>
2020-09-12 11:25:22 +0530

    NUKE MD5 setup

--- build/tasks/kronic.mk
+++ build/tasks/kronic.mk
@@ -4,11 +4,9 @@ YAAP_TARGET_PACKAGE := $(PRODUCT_OUT)/YAAP-$(YAAP_VERSION).zip
 otapackage: $(INTERNAL_OTA_PACKAGE_TARGET)
 kronic: otapackage
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(YAAP_TARGET_PACKAGE)
-	$(hide) $(MD5SUM) $(YAAP_TARGET_PACKAGE) | cut -d ' ' -f1 > $(YAAP_TARGET_PACKAGE).md5sum
 	@echo -e ""
 	@echo -e "${cya}Building ${bldcya}YAAP OS ${txtrst}";
 	@echo -e "zip: "$(YAAP_TARGET_PACKAGE)
-	@echo -e "md5: `cat $(YAAP_TARGET_PACKAGE).md5sum | cut -d ' ' -f 1`"
 	@echo -e "size:`ls -lah $(YAAP_TARGET_PACKAGE) | cut -d ' ' -f 5`"
 	@echo -e ""
 
