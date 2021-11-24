613ee82c626968ad6d479f8a346bf339db498257
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-11-25 02:03:34 +0530

    config: drop blur
    * to be revisited
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- config/common.mk
+++ config/common.mk
@@ -19,12 +19,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     net.hostname=$(TARGET_VENDOR_DEVICE_NAME)
 
-#Blurr
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.sf.blurs_are_expensive=1 \
-    ro.surface_flinger.supports_background_blur=1 \
-    persist.sys.sf.disable_blurs=1
-
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/yaap/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
