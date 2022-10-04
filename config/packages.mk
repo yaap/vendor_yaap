613d39deb3074f663740e9fc26396095da0e5e91
Nolen Johnson <johnsonnolen@gmail.com>
2022-10-03 21:16:56 -0400

    vendor: packages: Exclude ntfs-3g from artifact path requirement
    idoybh: also add some other tools we were missing
    
    Change-Id: I6253ac0b326f86ba7fd78f6babb9ee2242ba9133

--- config/packages.mk
+++ config/packages.mk
@@ -37,6 +37,18 @@ PRODUCT_PACKAGES += \
     OpenDelta \
     PhotonCamera
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/curl \
+    system/bin/getcap \
+    system/bin/setcap
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/fsck.ntfs \
+    system/bin/mkfs.ntfs \
+    system/bin/mount.ntfs \
+    system/%/libfuse-lite.so \
+    system/%/libntfs-3g.so
+
 ifneq ($(TARGET_BUILD_GAPPS),true)
 PRODUCT_PACKAGES += \
     Apps \
