c894de15db9a31020a0fe97bfcbd992a3e6c772b
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-02 21:57:53 +0200

    vendor: Conditionally enable dev keys signing

--- config/version.mk
+++ config/version.mk
@@ -29,3 +29,17 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
   ro.yaap.version=$(YAAP_VERSION) \
   ro.yaap.device=$(YAAP_BUILD) \
   ro.modversion=$(YAAP_VERSION)
+
+# Signing
+ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard .android-certs/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard .android-certs/verity.pk8))
+PRODUCT_VERITY_SIGNING_KEY := .android-certs/verity
+endif
+ifneq (,$(wildcard .android-certs/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+endif
+endif
