f0959a6985f02fd39af7b562d9451e639af0a7a1
Dyneteve <dyneteve@hentaios.com>
2023-09-12 10:31:04 -0400

    config/packages: Build the textclassifier models

--- config/packages.mk
+++ config/packages.mk
@@ -42,6 +42,13 @@ PRODUCT_PACKAGES += \
     Seedvault \
     OpenDelta
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_annotator_en_model \
+    libtextclassifier_annotator_universal_model \
+    libtextclassifier_actions_suggestions_universal_model \
+    libtextclassifier_lang_id_model
+
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/curl \
     system/bin/getcap \
