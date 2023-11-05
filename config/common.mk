de7ba56898abda44abf6e99e45d287cfcdbaf74d
Cyber Knight <cyberknight755@gmail.com>
2023-11-05 07:33:50 +0000

    yaap: Inherit device/qcom/common
    - Also include QCOM core-utils and exclude QCOM SEPolicy.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -41,6 +41,11 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 # Dedupe VNDK libraries with identical core variants
 TARGET_VNDK_USE_CORE_VARIANT := true
 
+# Inherit device/qcom/common, QCOM core-utils and exclude QCOM SEPolicy
+TARGET_EXCLUDE_QCOM_SEPOLICY := true
+$(call inherit-product, device/qcom/common/common.mk)
+include vendor/qcom/opensource/core-utils/build/utils.mk
+
 # Use a generic profile based boot image by default
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := art/build/boot/boot-image-profile.txt
