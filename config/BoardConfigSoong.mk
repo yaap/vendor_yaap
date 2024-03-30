2ea486ec9b862e60100429c39147556c1ab6c782
Cyber Knight <cyberknight755@gmail.com>
2024-03-30 11:42:30 +0000

    soong-gen: Partially Revert "d982199c5 [DNM] soong: Fully override path for now"
    - Also remove MAKE_PREBUILT as it is unused.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -11,9 +11,7 @@ EXPORT_TO_SOONG := \
     KERNEL_MAKE_FLAGS \
     PATH_OVERRIDE_SOONG \
     TARGET_KERNEL_CONFIG \
-    TARGET_KERNEL_SOURCE \
-    KERNEL_CC \
-    MAKE_PREBUILT
+    TARGET_KERNEL_SOURCE
 
 # Setup SOONG_CONFIG_* vars to export the vars listed above.
 # Documentation here:
