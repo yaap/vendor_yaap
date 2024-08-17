908f4ef871bccc3b3053998af1d42da713a72697
Ido Ben-Hur <idoybh2@gmail.com>
2024-08-17 03:52:16 +0300

    generate_json_build_info.sh: Account for A-only
    refactor this script while at it

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+RED="\033[1;31m"
 GREEN="\033[1;32m"
 YELLOW="\033[1;33m"
 NC="\033[0m"
+
+OFFICIAL_TAGS=("Urshanabi" "Vanilla")
 
 findPayloadOffset() {
     build=$1
@@ -24,45 +27,77 @@ findPayloadOffset() {
     done <<< "$info"
 }
 
-if [ "$1" ]; then
-    echo "Generating .json"
-    file_path=$1
-    file_dir=$(dirname "$file_path")
-    file_name=$(basename "$file_path")
-
-    if [ -f $file_path ]; then
-        # only generate for official builds. unless forced with 'export FORCE_JSON=1'
-        if [[ $file_name == *"Urshanabi"* ]] || [[ $file_name == *"Vanilla"* ]] || [[ $FORCE_JSON == 1 ]]; then
-            if [[ $FORCE_JSON == 1 ]]; then
-                echo -e "${GREEN}Forced generation of json${NC}"
-            fi
-            offset=$(findPayloadOffset "$file_path")
-            [ -f payload_properties.txt ] && rm payload_properties.txt
-            unzip "$file_path" payload_properties.txt
-            keyPairs=$(cat payload_properties.txt | sed "s/=/\": \"/" | sed 's/^/          \"/' | sed 's/$/\"\,/')
-            keyPairs=${keyPairs%?}
-            datetime=$(date +%s)
-            {
-                echo "{"
-                echo "  \"response\": ["
-                echo "    {"
-                echo "      \"datetime\": ${datetime},"
-                echo "      \"filename\": \"${file_name}\","
-                echo "      \"payload\": ["
-                echo "        {"
-                echo "          \"offset\": ${offset},"
-                echo "${keyPairs}"
-                echo "        }"
-                echo "      ]"
-                echo "    }"
-                echo "  ]"
-                echo "}"
-            } > $file_path.json
-            device_code=$(echo $file_name | cut -d'-' -f4)
-            mv "${file_path}.json" "${file_dir}/${device_code}.json"
-            echo -e "${GREEN}Done generating ${YELLOW}${device_code}.json${NC}"
-        else
-            echo -e "${YELLOW}Skipped generating json for a non-official build${NC}"
-        fi
-    fi
+if ! [ "$1" ]; then
+    echo "${RED}No file provided${NC}"
+    return 1
 fi
+
+file_path=$1
+file_dir=$(dirname "$file_path")
+file_name=$(basename "$file_path")
+
+if ! [ -f "$file_path" ]; then
+    echo "${RED}File does not exist${NC}"
+    return 1
+fi
+
+# only generate for official builds. unless forced with 'export FORCE_JSON=1'
+isOfficial=0
+if [[ $FORCE_JSON == 1 ]]; then
+    isOfficial=1
+    echo -e "${GREEN}Forced generation of json${NC}"
+else
+    for tag in "${OFFICIAL_TAGS[@]}"; do
+        if [[ $file_name == *"${tag}"* ]]; then
+            isOfficial=1
+            break
+        fi
+    done
+fi
+
+if [[ $isOfficial != 1 ]]; then
+    echo -e "${YELLOW}Skipped generating json for a non-official build${NC}"
+    return 1
+fi
+
+echo "${GREEN}Generating .json${NC}"
+
+isPayload=0
+[ -f payload_properties.txt ] && rm payload_properties.txt
+if unzip "$file_path" payload_properties.txt; then
+    isPayload=1
+    offset=$(findPayloadOffset "$file_path")
+    keyPairs=$(cat payload_properties.txt | sed "s/=/\": \"/" | sed 's/^/          \"/' | sed 's/$/\"\,/')
+    keyPairs=${keyPairs%?}
+fi
+datetime=$(date +%s)
+
+{
+    echo    "{"
+    echo    "  \"response\": ["
+    echo    "    {"
+    echo    "      \"datetime\": ${datetime},"
+    echo -n "      \"filename\": \"${file_name}\""
+} > "${file_path}.json"
+if [[ $isPayload == 1 ]]; then
+    {
+        echo ","
+        echo "      \"payload\": ["
+        echo "        {"
+        echo "          \"offset\": ${offset},"
+        echo "${keyPairs}"
+        echo "        }"
+        echo "      ]"
+    } >> "${file_path}.json"
+else
+        echo "" >> "${file_path}.json"
+fi
+{
+        echo "    }"
+        echo "  ]"
+        echo "}"
+} >> "${file_path}.json"
+
+device_code=$(echo "${file_name}" | cut -d'-' -f4)
+mv "${file_path}.json" "${file_dir}/${device_code}.json"
+echo -e "${GREEN}Done generating ${YELLOW}${device_code}.json${NC}"
