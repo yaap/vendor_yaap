8d66b0e8bc810fda76803424d57297a1072eaa44
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2026-04-25 19:00:06 +0530

    config: Inherit correct phone target for tablets
    !fixup vendor: Import tablet specific configs and

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -1,5 +1,5 @@
-# Inherit mobile full common stuff
-$(call inherit-product, vendor/yaap/config/common_mobile_full.mk)
+# Inherit full phone common stuff
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Inherit tablet common stuff
 $(call inherit-product, vendor/yaap/config/tablet.mk)
